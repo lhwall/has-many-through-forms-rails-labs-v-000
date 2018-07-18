2f31ff90828dc17928028c42f5e420419d17d9d8
Lucy Wall <lhwall@gmail.com>
2018-07-18 12:32:49 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], user_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], user_attributes: [:name], category_ids:[], categories_attributes: [:name])
   end
 end
