dcf56fba29c1436652b3d1e277228a0a10978257
Lucy Wall <lhwall@gmail.com>
2018-07-18 12:46:27 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,22 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+
+def categories_attributes=(category_attributes)
+  category_attributes.values.each do |category_attribute|
+    category = Category.find_or_create_by(category_attribute)
+    self.categories << category
+  end
+end
+
+def unique_users
+   users_array = []
+  self.comments.each do |comment|
+    users_array << comment.user
+  end
+  users_array.uniq
+end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def commented_posts
+    commented_on_posts = []
+    self.comments.each do |comment|
+      commented_on_posts << comment.post
+    end
+    commented_on_posts.uniq
+  end
+
 end
