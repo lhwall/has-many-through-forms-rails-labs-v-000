6474b79fee3f0ca4ca310a6aa89b8b6de9c493f4
Lucy Wall <lhwall@gmail.com>
2018-07-18 12:31:03 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ def categories_attributes=(category_attributes)
   category_attributes.values.each do |category_attribute|
     category = Category.find_or_create_by(category_attribute)
     self.categories << category
+    binding.pry
   end
 end
 
