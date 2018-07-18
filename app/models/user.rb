da96430537f017f368043b7af56132d61c7909ad
Lucy Wall <lhwall@gmail.com>
2018-07-18 12:46:04 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   def commented_posts
     commented_on_posts = []
     self.comments.each do |comment|
-      commented_on_posts << comment
+      commented_on_posts << comment.post
     end
     commented_on_posts.uniq
   end
